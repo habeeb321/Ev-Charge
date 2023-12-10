211643e04373ecb1a7546f0681a02030e6c44ec7
habeebu <habeebu@hello24.in>
2023-12-10 15:38:40 +0530

    splash screen completed

--- lib/splash_screen/splash_screen.dart
+++ lib/splash_screen/splash_screen.dart
@@ -1,13 +1,73 @@
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
+  const SplashScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Splash Screen'),
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SizedBox(
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: size.height * 0.4),
+                SizedBox(
+                  height: 70,
+                  child: Stack(
+                    children: const [
+                      Text.rich(
+                        TextSpan(
+                          children: [
+                            TextSpan(
+                              text: 'charge',
+                              style: TextStyle(fontSize: 50),
+                            ),
+                            TextSpan(
+                              text: 'MOD',
+                              style: TextStyle(
+                                fontSize: 50,
+                                color: Color(0xffEC6E00),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Positioned(
+                        right: 0,
+                        top: 50,
+                        child: Text(
+                          'Power to People',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w300,
+                            fontSize: 18,
+                            color: Color(0xffF29242),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const Spacer(),
+                SizedBox(
+                  width: size.width * 0.6,
+                  child: const LinearProgressIndicator(
+                    value: 0.25,
+                    color: Color(0xffF76C00),
+                    backgroundColor: Color(0xffEBEBEB),
+                    minHeight: 8,
+                  ),
+                ),
+                const SizedBox(height: 16),
+                const Text('Connecting to chargeMOD'),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
